5520d202319cabe7c50119a5fc9451686a1c8cf2
Alexey Arendarenko <alexeya@ven.com>
2019-03-19 11:02:28 +0200

    fix property scope

--- Controller/Adminhtml/Email/Test.php
+++ Controller/Adminhtml/Email/Test.php
@@ -25,7 +25,7 @@ class Test extends \Magento\Backend\App\Action
     protected $_helper;
 
     /** @var StoreManagerInterface */
-    public $storeManager;
+    private $storeManager;
 
     /**
      * Test constructor.
